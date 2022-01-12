1d153974908f28a09831f4588dd8958542ee51bc
TheDusKnight <860956020@qq.com>
2022-01-11 23:25:58 -0800

    1/11

--- leetcode/_0399_EvaluateDivision/Solution.java
+++ leetcode/_0399_EvaluateDivision/Solution.java
@@ -2,89 +2,103 @@ package leetcode._0399_EvaluateDivision;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class Solution {
-    private class V {
-        public String name;
-        public double val; // *
-        public V parent;
-        private int size;
-
+class Solution {
+    class V {
+        String name;
+        V parent;
+        int size;
+        double val;
+        
         public V(String name) {
             this.name = name;
             parent = this;
-            val = 1.0;
             size = 1;
+            val = 1.0;
         }
     }
-
-    private boolean find(V v1, V v2) {
-        return root(v1) == root(v2);
-    }
-
-    private void union(V v1, V v2, double d) { // d is v1, v2 division
-        V root1 = root(v1), root2 = root(v2);
-        if (root1.size > root2.size) {
-            root2.parent = root1;
-            root2.val = v1.val * d / v2.val;
-            root1.size += root2.size;
-        } else {
-            root1.parent = root2;
-            root1.val = v2.val / (v1.val * d);
-            root2.size += root1.size;
-        }
-    }
-
-    private V root(V v) {
-        V cur = v;
+    
+    private V getRoot(V p) {
+        V cur = p;
         double d = 1.0;
-        while (cur != cur.parent) {
+        while (cur.parent != cur) {
             cur.val *= cur.parent.val;
+            // 注意cur的下一个不是cur.parent而是cur.parent.paretn
+            // d要保证乘以所有cur.val所以一轮乘以cur.val和cur.parent.val
             d *= cur.val;
             cur.parent = cur.parent.parent;
             cur = cur.parent;
         }
-        v.parent = cur;
-        v.val = d;
+        p.val = d;
+        p.parent = cur;
+        
         return cur;
     }
-
-    private double division(V v1, V v2) {
-        return v2.val / v1.val;
+    
+    public boolean find(V p, V q) {
+        return getRoot(p) == getRoot(q);
     }
-
+    
+    public void union(V p, V q, double d) { // d is v1, v2 division
+        V rootP = getRoot(p), rootQ = getRoot(q);
+        if (rootP.size > rootQ.size) {
+            rootQ.val = p.val * d / q.val;
+            rootQ.parent = rootP;
+            rootP.size += rootQ.size;
+        } else {
+            rootP.val = q.val / (p.val * d);
+            rootP.parent = rootQ;
+            rootQ.size += rootP.size;
+        }
+    }
+    
+    private double division(V p, V q) {
+        // v1.val = |p|/|v1|; v2.val = |p|/|v2|;
+        // v2.val / v1.val = |v1| / |v2|
+        return q.val / p.val;
+    }
+    
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        int len = equations.size(), lenQ = queries.size();
-        double[] res = new double[lenQ];
-        HashMap<String, V> map = new HashMap<>();
-        for (int i = 0; i < len; i++) {
-            List<String> equa = equations.get(i);
-            String s1 = equa.get(0), s2 = equa.get(1);
+        // cc
+        int equLen = equations.size(), queLen = queries.size();
+        Map<String, V> map = new HashMap<>();
+        // construct UF
+        for (int i = 0; i < equLen; i++) {
+            List<String> equ = equations.get(i);
+            String s0 = equ.get(0), s1 = equ.get(1);
             double val = values[i];
-            if (!map.containsKey(s1)) map.put(s1, new V(s1));
-            if (!map.containsKey(s2)) map.put(s2, new V(s2));
-
-            V v1 = map.get(s1);
-            V v2 = map.get(s2);
-            if (!find(v1, v2)) {
-                union(v1, v2, val);
+            
+            V v0 = map.get(s0), v1 = map.get(s1);
+            if (v0 == null) {
+                v0 = new V(s0);
+                map.put(s0, v0);
+            }
+            if (v1 == null) {
+                v1 = new V(s1);
+                map.put(s1, v1);
+            }
+            if (!find(v0, v1)) {
+                union(v0, v1, val);
             }
         }
-
-        for (int i = 0; i < lenQ; i++) {
-            List<String> query = queries.get(i);
-            String s1 = query.get(0), s2 = query.get(1);
-            if (!map.containsKey(s1) || !map.containsKey(s2)) {
+        // cal query
+        double[] res = new double[queLen];
+        for (int i = 0; i < queLen; i++) {
+            List<String> que = queries.get(i);
+            String q0 = que.get(0), q1 = que.get(1);
+            V v0 = map.get(q0), v1 = map.get(q1);
+            if (v0 == null || v1 == null) {
                 res[i] = -1.0;
+                continue;
+            }
+            if (find(v0, v1)) {
+                res[i] = division(v0, v1);
             } else {
-                V v1 = map.get(s1), v2 = map.get(s2);
-                if (find(v1, v2)) {
-                    res[i] = division(v1, v2);
-                } else {
-                    res[i] = -1.0;
-                }
+                res[i] = -1.0;
             }
         }
+        
         return res;
     }
 }
